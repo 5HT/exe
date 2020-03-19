39f14adaed24b48162c3f3ed96b3a4ae828c236d
Namdak Tonpa <maxim@synrc.com>
2020-03-20 01:43:07 +0200

    accumulate error codes

--- src/homotopy.erl
+++ src/homotopy.erl
@@ -6,14 +6,18 @@
 -export([init/1, start/2, stop/1, main/1, console/1]).
 
 init([])   -> {ok, {{one_for_one, 5, 10}, []}}.
-main(A)    -> console:unicode(), case A of [] -> halt(help()); A -> halt(lists:sum(console(A))) end.
+main(A)    -> console:unicode(), case A of [] -> halt(help()); A -> halt(console(A)) end.
 start(_,_) -> console:unicode(), supervisor:start_link({local,?MODULE},?MODULE,[]).
 stop(_)    -> ok.
 
+errcode({error,_}) -> 1;
+errcode(_)         -> 0.
+
 console(S) ->
-  lists:foldr(fun(I,_) -> R = lists:reverse(I),
-    io:format("~tp~n",[lists:foldl(fun(X,A) -> console:(list_to_atom(lists:concat([X])))(A) end,hd(R),tl(R))]),
-    [] end, [], string:tokens(S,[","])).
+  lists:foldr(fun(I,Errors) -> R = lists:reverse(I),
+    Res = lists:foldl(fun(X,A) -> console:(list_to_atom(lists:concat([X])))(A) end,hd(R),tl(R)),
+    io:format("~tp~n",[Res]),
+    Errors + errcode(Res) end, 0, string:tokens(S,[","])).
 
 help() ->
   io:format("CTT-CCHM Homotopy Type System ~s~n~n",[proplists:get_value(vsn,module_info(attributes))]),
